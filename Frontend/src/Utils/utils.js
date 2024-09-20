6d9230990329e824a8cbe86fe21497c8aac602b7
Md Ismail <mdismaeelkhan345@gmail.com>
2024-09-20 10:03:38 +0530

    reset and newpassword page added

--- Frontend/src/Utils/utils.js
+++ Frontend/src/Utils/utils.js
@@ -1,4 +1,3 @@
-/* Backend apis */
 
 // export const BACKEND_API_ENDPOINTS = `http://localhost:10000/api/v1/user`
 export const BACKEND_API_ENDPOINTS = `https://kodekalp-assignment.onrender.com/api/v1/user`
